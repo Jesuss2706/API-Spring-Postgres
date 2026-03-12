5ca14215fee0dd49302311a2ce11c35846cfed5c
Jesuss2706 <jeslamu2006@gmail.com>
2026-03-12 01:01:23 -0500

    initialize proyect

---
+++ spring-postgres-api/src/main/java/com/unicauca/spring_postgres_api/SpringPostgresApiApplication.java
@@ -0,0 +1,13 @@
+package com.unicauca.spring_postgres_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostgresApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPostgresApiApplication.class, args);
+	}
+
+}
---
+++ spring-postgres-api/src/test/java/com/unicauca/spring_postgres_api/SpringPostgresApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicauca.spring_postgres_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostgresApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
